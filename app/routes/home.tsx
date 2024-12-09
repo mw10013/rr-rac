c7ab2a6bb4d73fdc170906083702b0bbb6b1667c
Michael Wu <mw10013@gmail.com>
2024-12-09 17:11:03 -0500

    button

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,5 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { Button } from "react-aria-components";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,5 +9,18 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <div className="container p-6">
+      <div className="bg-gradient-to-r from-teal-300 to-cyan-500 p-12 rounded-lg flex justify-center">
+        <Button
+          className={`
+      relative overflow-hidden
+      inline-flex items-center justify-center rounded-md bg-black bg-opacity-50 bg-clip-padding border border-white/20 px-6 py-4 text-white text-lg
+      hover:bg-opacity-60 pressed:bg-opacity-70 transition-colors cursor-default outline-none focus-visible:ring-2 focus-visible:ring-white/75`}
+        >
+          Book flight
+        </Button>
+      </div>
+    </div>
+  );
 }
